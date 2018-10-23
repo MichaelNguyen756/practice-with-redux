f27770bf03db0a96ce798dce644ecf91dfbd5682
MichaelNguyen756 <michael.nguyen756@gmail.com>
2018-10-23 20:16:38 +1100

    Update .DS_Store

--- BasicReactWithRedux/my-app/src/index.js
+++ BasicReactWithRedux/my-app/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from './Redux/actions';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -20,22 +14,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 const store = createStore(todoApp);
-
-// Log the initial state
-console.log(store.getState());
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() =>
-  console.log(store.getState())
-);
-
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-// Stop listening to state updates
-unsubscribe();
